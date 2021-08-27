f7702cf47271f792e35172766396d91131d243cf
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-27 17:37:56 +0300

    added buttons to the browser

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://github.com/Theuri0411"))
+        self.browser.setUrl(QUrl("https://google.com"))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
@@ -19,6 +19,15 @@ class MainWindow(QMainWindow):
         back_button = QAction("Back", self)
         back_button.triggered.connect(self.browser.back)
         navbar.addAction(back_button)
+
+        forward_button = QAction("forward", self)
+        forward_button.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_button)
+
+        reload_button = QAction("Reload", self)
+        reload_button.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_button)
+
 
 
 app = QApplication(sys.argv)
