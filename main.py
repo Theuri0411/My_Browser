8f0588b27d261e2bcb083a848da2e24e105bcfdb
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-27 14:35:17 +0300

    set the URL

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import sys
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://github.com/Theuri0411"))
+        self.setCentralWidget(self.browser)
         self.showMaximized()
 
-        
+
 app = QApplication(sys.argv)
 QApplication.setApplicationDisplayName("My_Browser")
 window = MainWindow()
