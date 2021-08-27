6541eac256e2356cab3f4513daeb7a205d40b865
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-27 21:01:18 +0300

    Added home button

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://google.com"))
+        self.browser.setUrl(QUrl("https://github.com/Theuri0411"))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
@@ -16,21 +16,27 @@ class MainWindow(QMainWindow):
         navbar = QToolBar()
         self.addToolBar(navbar)
 
-        back_button = QAction("Back", self)
+        back_button = QAction("BACKWARD", self)
         back_button.triggered.connect(self.browser.back)
         navbar.addAction(back_button)
 
-        forward_button = QAction("forward", self)
+        forward_button = QAction("FORWARD", self)
         forward_button.triggered.connect(self.browser.forward)
         navbar.addAction(forward_button)
 
-        reload_button = QAction("Reload", self)
+        reload_button = QAction("REFRESH", self)
         reload_button.triggered.connect(self.browser.reload)
         navbar.addAction(reload_button)
 
+        home_button = QAction("HOME", self)
+        home_button.triggered.connect(self.navigate_home)
+        navbar.addAction(home_button)
+
+        def navigate_home(self):
+            self.browser.setUrl(QUrl("https://google.com"))
 
 
 app = QApplication(sys.argv)
-QApplication.setApplicationDisplayName("My_Browser")
+QApplication.setApplicationDisplayName("MY_BROWSER")
 window = MainWindow()
 app.exec_()
